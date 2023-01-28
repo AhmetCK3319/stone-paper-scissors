155655484aa66077a27e693f86370850b84ca2ec
Ahmet Can KOCA <ahmetcankoca1990@hotmail.com>
2023-01-28 18:55:33 +0300

    ı harfleri i harfi olarak düzeltildi

--- tas_kagıt_makas.py
+++ tas_kagıt_makas.py
@@ -7,7 +7,7 @@ while True:
     secenek=["taş","kağit","makas"]
     #YANLIŞ YAPILABİLECEK SEÇİMLER LİSTE İÇERİSİNE GİRİLEREK İF YAPISINI KULLANMAYA GEREK KALMADI.
     secimler={ "Taş":0,"tas":0,"TAS":0,"TAŞ":0,"Tas":0,"taş":0,
-                "kagit":1,"KAGIT":1,"KAĞIT":1,"Kagıt":1,"kağit":1,"kağıt":1,
+                "kagit":1,"KAGIT":1,"KAĞIT":1,"Kagit":1,"kağit":1,"kağit":1,
                 "Makas":2,"MAKAS":2,"makas":2   
     }
     #bilgisayarin seçim yapmasi için kodlarimizi yaziyoruz.
@@ -16,23 +16,23 @@ while True:
     #kullanicinin seçim yapmasini sağliyoruz
     usersecim=input("bir seçenek yapiniz(taş,kağit,makas) :")
     usersecim=secimler.get(usersecim)
-    #kazanma durumları belirtiliyor
+    #kazanma durumlari belirtiliyor
     if usersecim==bilsecim:
         print("berabere")
     elif usersecim==2 and  bilsecim==1:
-        print("kazandınız")
+        print("kazandiniz")
         userscore+=1
     elif usersecim==1 and bilsecim==0:
-        print("kazandınız")
+        print("kazandiniz")
         userscore+=1
     elif usersecim==0 and bilsecim==2:
-        print("kazandınız")
+        print("kazandiniz")
         userscore+=1        
     else:
         print("kaybettiniz")
         bilscore+=1        
-    print(secenek [bilsecim], "= bilgisayar puanı :",bilscore)
-    print(secenek [usersecim],"= sizin puanınız :",userscore)
+    print(secenek [bilsecim], "= bilgisayar puani :",bilscore)
+    print(secenek [usersecim],"= sizin puaniniz :",userscore)
    
     if userscore==3 :
         print("TEBRİKLER OYUNU KAZANDINIZ")
